33995d46a047926ccc3feb4a163aaabd696a63f5
sf <sreedfenton@gmail.com>
2022-01-22 02:34:37 +0000

    order of an element program is added and functioning!

--- orderOfAnElement.py
+++ orderOfAnElement.py
@@ -9,17 +9,6 @@ def integerCheck(integer):                          #takes an input and returns 
     else:
         return True
 
-def continuance():                                  #creates a loop which prompts the user to continue or exit
-    while True:
-        continuance = input("do you want to do another calculation? \n enter yes or no \n ")
-        if continuance.lower().strip == "yes":
-            break
-        elif continuance.lower().strip == "no":
-            print("\n thank you \n")
-            sys.exit()
-        else:
-            print("your input was not valid")
-
 def euclideanAlgorithm(firstNumber, secondNumber):   #takes two inputs and returns the GCD
     remainder = None
     firstNumber = int(firstNumber)
@@ -46,18 +35,51 @@ def modularReduction(modulus, a):                   #takes two inputs and return
 
     return r
 
+def continuance():
+    while True:
+            continuance = input("do you want to do another calculation? \n enter yes or no \n ")
+            if continuance.lower().strip() == "yes":
+                break
+            elif continuance.lower().strip() == "no":
+                print("\n thank you \n")
+                sys.exit()
+            else:
+                print("your input was not valid")
+
+while True:
+    while True:
+        group = input("enter a group: ")
+        if integerCheck(group) == False:
+            continue
+        elif int(group) == 0:
+            print("the group must not be 0.")
+            continue
+        else:
+            break
+    cyclicGroup = []
+    for eachElement in range (1, int(group)):
+        if euclideanAlgorithm(int(eachElement), group) == 1:
+            cyclicGroup.append(int(eachElement))
+    while True:
+        element = input("enter an element of that group: ")
+        if integerCheck(element) == False:
+            continue
+        element = int(element)
+        if element not in cyclicGroup:
+            print("Sorry. The element ", element, " is not in the finite group ", group)
+            print(cyclicGroup, "are the elements in the finite group ", group)
+            continue
+        else:
+            break
+    remainder = None
+    exponent = 0
+    while remainder != 1:
+        exponent += 1
+        remainder = modularReduction(group, int(element) ** exponent)
+    print("the order of ", element, " in the group ", group," is ", exponent)
+
+    continuance()
 
 
 
-
-group = input("enter a group: ")
-element = input("enter an element of that group: ")
-
-cyclicGroup = []
-for eachElement in range (1, int(group)):
-    if euclideanAlgorithm(int(eachElement), group) == 1:
-        cyclicGroup.append(eachElement)
-if element not in cyclicGroup:
-    print("Sorry. The element ", element, " is not in the finite group ", group)
-    print(cyclicGroup, "are the elements in the finite group ", group)
-
+sys.exit()
